357d2588b25100933eedca824f7dd8fc4fb42f45
向阳 <xiangqian_liu@163.com>
2022-06-19 17:34:47 +0800

    feat: test slot

--- packages/components/src/components/xy-button/test/xy-button.spec.tsx
+++ packages/components/src/components/xy-button/test/xy-button.spec.tsx
@@ -1,18 +1,30 @@
+import { h } from '@stencil/core'
 import { newSpecPage } from '@stencil/core/testing'
 import { XyButton } from '../xy-button'
 
 describe('xy-button', () => {
-	it('renders', async () => {
+	it('should render xy-button component', async () => {
 		const page = await newSpecPage({
 			components: [XyButton],
-			html: `<xy-button></xy-button>`
+			template: () => <xy-button></xy-button>
 		})
 		expect(page.root).toEqualHtml(`
       <xy-button>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
+        <button class="xy-button primary medium"></button>
       </xy-button>
+    `)
+	})
+
+	it('should has a "hello world" as innerText', async () => {
+		const content = 'hello world'
+		const page = await newSpecPage({
+			components: [XyButton],
+			template: () => <xy-button>{content}</xy-button>
+		})
+		expect(page.root).toEqualHtml(`
+    <xy-button>
+      <button class="xy-button primary medium">hello world</button>
+    </xy-button>
     `)
 	})
 })
