c9211a8fd75c99f98994548c3eb361e536814cb4
向阳 <xiangqian_liu@163.com>
2022-06-19 09:16:14 +0800

    fix: 修改一个类型

--- packages/components/src/components/xy-button/types.ts
+++ packages/components/src/components/xy-button/types.ts
@@ -1,2 +1,2 @@
-export type Type = 'default' | 'primary' | 'info' | 'success' | 'warning' | 'error'
+export type Type = 'default' | 'primary' | 'info' | 'success' | 'warning' | 'danger'
 export type Size = 'mini' | 'small' | 'medium' | 'large'
